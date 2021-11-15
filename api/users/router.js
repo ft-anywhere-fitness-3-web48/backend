fc2a62efbee3d38f2efdb9e466afcacd6f7d68d2
Sebastian <sebas6292@gmail.com>
2021-11-15 11:34:08 -0800

    wip

---
+++ api/users/router.js
@@ -0,0 +1,40 @@
+// const express = require('expresss')
+
+// const router = express.Router()
+
+// // router.get('/', (req, res, next) => {
+
+// // })
+
+// // router.get('/:id', (req, res, next) => {
+    
+// // })
+
+// // router.post('/', (req, res, next) => {
+    
+// // })
+
+// // router.post('/:id', (req, res, next) => {
+    
+// // })
+
+// // router.put('/', (req, res, next) => {
+    
+// // })
+
+// // router.put('/:id', (req, res, next) => {
+    
+// // })
+
+// // router.delete('/', (req, res, next) => {
+    
+// // })
+
+// router.use((err, req, res, next) => { //eslint-disable-line
+//     res.status(err.status || 500).json({
+//         message: err.message,
+//         stack:err.stack,
+//     })
+// })
+
+// module.exports = router
